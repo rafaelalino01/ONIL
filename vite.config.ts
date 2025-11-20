994fcee469cc8372a59bcb4c77b3b20ee5fc46de
rafaelalino01 <rafalinno01@gmail.com>
2025-11-20 16:13:28 -0300

    Preparando deploy site

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,30 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+
+  // base ajustada para o nome do repositório
+  base: "/ONIL/",
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
     },
   },
 }));
